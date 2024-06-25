73b57eec7af469bf18c51d44f53c73d970f00062
Kammi Henderson <kammi_henry@yahoo.com>
2024-06-25 14:53:25 -0500

    Remaining guesses

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const guessLetterButton = document.querySelector(".guess");
 const letterInput= document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
 const remainingGuessesElement = document.querySelector(".remaining");
-const remainingGuessesSpan = document.querySelector(".remaining-span");
+const remainingGuessesSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
